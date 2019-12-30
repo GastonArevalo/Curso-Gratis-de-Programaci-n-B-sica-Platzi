ae099e21b47b083e67aa5c59b0abe6618edb79e0
Gaston Arevalo <gastonarevalo@Gastons-MacBook-Pro.local>
2019-12-29 21:11:25 -0300

    dibujar cantidad de lineas que ingresa por formulario

--- canvas/dibujo.js
+++ canvas/dibujo.js
@@ -1,38 +1,38 @@
 var texto = document.getElementById("texto_lineas");
 var boton = document.getElementById("botoncito");
-boton.addEventListener("click", dibujoPorClick);
-
-var d = document.getElementById("dibujito");
-var lienzo = d.getContext("2d");
-var lineas = 30;
-var l = 0;
+boton.addEventListener("click", dibujoPorClick); //detecta evento por click
+//configuracion del dibujo curva de canvas
+var d = document.getElementById("dibujito"); //se importa el tamanño del canvas
+var lienzo = d.getContext("2d"); //se establece que el lienzo va a ser en 2 dimensiones
+var lineas = 0; // cantidad de lineas que va a conformar el dibujo de la curva
+var l = 0;  //inicializa la variable previo al bucle
 var yi, xf;
-var colorcito = "#FAA";
-
-while (l<lineas){
-    yi = 10 * l;
-    xf = 10 * (l +1);
-    dibujarLinea("#AAF", 0, yi, xf, 300);
-    console.log("Linea " + l);
-    l++;
-}
-dibujarLinea(colorcito, 1, 1, 1, 300);
-dibujarLinea(colorcito, 1, 299, 299, 299);
+var colorcito = "#FAA"; //establece el valor de un color para el contorno del canvas
 
 
+dibujarLinea(colorcito, 1, 1, 1, 300);   //contorno lateral
+dibujarLinea(colorcito, 1, 299, 299, 299); //contorno inferior
+
+//funcion para dibujar linea
 function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal)
 {
-    lienzo.beginPath();
-    lienzo.strokeStyle = color;
-    lienzo.moveTo(xinicial, yinicial);
-    lienzo.lineTo(xfinal, yfinal);
-    lienzo.stroke();
-    lienzo.closePath();
+    lienzo.beginPath();         //inicializacion del trazo
+    lienzo.strokeStyle = color; //establece el color del dibujo
+    lienzo.moveTo(xinicial, yinicial); //inicializa la ubicacion dentro del canvas donde va a comenzar el trazo
+    lienzo.lineTo(xfinal, yfinal); //ubicacion donde termina el trazo
+    lienzo.stroke();     //finaliza el trazo       
+    lienzo.closePath();     //finalizacion del dibujo
 }
 
 function dibujoPorClick()
 {
-    var x = texto.value;
-    alert("funka " + x);
-    console.log(texto);
+    var lineas = parseInt(texto.value);
+    console.log(texto.value);
+    while (l<lineas){
+        yi = 10 * l;
+        xf = 10 * (l +1);
+        dibujarLinea("#AAF", 0, yi, xf, 300);
+        console.log("Linea " + l);
+        l++;
+    }
 }
